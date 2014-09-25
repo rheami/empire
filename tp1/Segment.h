6a6af59225820b3b7a364382d7c91029ab816a05
granyte <granyte@hotmail.com>
2014-09-25 15:23:28 -0400

    segment stream out methode

--- tp1/Segment.h
+++ tp1/Segment.h
@@ -15,6 +15,10 @@ public:
     double distancePointSegment(Point& pointA ,const Segment& segmentCD);
     Point getProjection(Point& point) const;
 
+	friend std::ostream& operator << (std::ostream& os, const Segment& segement) {
+		os << "(" << segement.debut << "," << segement.fin << ")";
+		return os;
+	}
 private:
 	Point debut;
 	Point fin;
